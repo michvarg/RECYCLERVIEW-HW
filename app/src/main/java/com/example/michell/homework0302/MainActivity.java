4971320d5d56160dd7927c72dba0e68028da8466
Michell Vargas <michell@Michells-MacBook-Pro.local>
2019-01-19 17:42:36 -0500

    pushing recyclerview task

---
+++ app/src/main/java/com/example/michell/homework0302/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.michell.homework0302;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: started.");
+
+        initImageBitmap();
+    }
+
+    private void initImageBitmap(){
+        Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
+
+        mImageUrls.add("https://c1.staticflickr.com/5/4636/25316407448_de5f183d_o.jpf");
+        mNames.add("Havasu falls");
+
+        mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
+        mNames.add("Trondheim");
+
+        mImageUrls.add("https://i.redd.it/qn7f9oqu7o501.jpg");
+        mNames.add("Portugal");
+
+        mImageUrls.add("https://i.redd.it/j6myfglup501.jpg");
+        mNames.add("Rocky Mountain National Park");
+
+        mImageUrls.add("https://i.redd.it/k98uz168eh501.jpg");
+        mNames.add("Frozen Lake");
+
+        mImageUrls.add("https://i.redd.it/glin0nwndo501.jpg");
+        mNames.add("White Sands Desert");
+
+        mImageUrls.add("https://i.redd.it/obx4zydshg601.jpg");
+        mNames.add("Austrailia");
+
+        mImageUrls.add("https://i.redd.it/ZcLLrkY.jpg");
+        mNames.add("Washington");
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        Log.d(TAG, "initRecyclerView: inti recyclerview.");
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mNames, mImageUrls);
+
+
+    }
+}
