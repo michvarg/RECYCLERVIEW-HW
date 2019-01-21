9144f4a75a376373693dd1afb01de741404a92dc
Michell Vargas <michell@Michells-MacBook-Pro.local>
2019-01-21 14:30:12 -0500

    pushing

--- app/src/main/java/com/example/michell/homework0302/MainActivity.java
+++ app/src/main/java/com/example/michell/homework0302/MainActivity.java
@@ -1,18 +1,30 @@
 package com.example.michell.homework0302;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int NOTIFICATION_ID = 0;
+
+
     private static final String TAG = "MainActivity";
 
     private ArrayList<String> mNames = new ArrayList<>();
     private ArrayList<String> mImageUrls = new ArrayList<>();
+    RecyclerView recyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: started.");
 
         initImageBitmap();
+
+        createNotificationChannel();
     }
 
     private void initImageBitmap(){
@@ -55,9 +69,54 @@ public class MainActivity extends AppCompatActivity {
 
     private void initRecyclerView(){
         Log.d(TAG, "initRecyclerView: inti recyclerview.");
-        RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mNames, mImageUrls);
+        recyclerView = findViewById(R.id.recycler_view);
+        RecyclerViewAdapter recyclerViewAdapter= new RecyclerViewAdapter(mNames, mImageUrls,this);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.setAdapter(recyclerViewAdapter);
+        recyclerView.setLayoutManager(linearLayoutManager);
+    }
+
+    private Button button_notify;
+
+    public void setNotificationId(){}
+
+
+
+
+    public void setButton_notify(Button button_notify) {
+        this.button_notify = button_notify;
+    }
+
+    private static final String PRIMARTY_CHANNEL_ID = "primary_notification_channel";
+
+    private NotificationManager mNotifyManger;
+
+    public NotificationCompat.Builder createNotificationChannel(){
+
+        mNotifyManger = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);               // create a notificationchannel
+          if (Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+
+              NotificationChannel notificationChannel = new NotificationChannel(PRIMARTY_CHANNEL_ID, "Vacation Notification", NotificationManager.IMPORTANCE_HIGH );
+
+          }
+
+         NotificationCompat.Builder getNotificationBuilder; {
+            NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this, PRIMARTY_CHANNEL_ID)
+                    .setContentTitle("you've been notified")
+                    .setContentText("This is your notification text.")
+                    .setSmallIcon(R.drawable.ic_android);
+            return notifyBuilder;
+
+
+
+        }
+
+
 
 
     }
+
+
+
 }
--- app/src/main/java/com/example/michell/homework0302/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/michell/homework0302/RecyclerViewAdapter.java
@@ -1,5 +1,8 @@
+package com.example.michell.homework0302;
+
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,7 +13,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.example.michell.homework0302.R;
 
 import java.util.ArrayList;
 
@@ -18,10 +20,10 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
 
-    private static final String TAG = "RecyclerViewAdapter" ;
+    private static final String TAG = "com.example.michell.homework0302.RecyclerViewAdapter" ;
 
-    private ArrayList<String> mImageNames = new ArrayList<>();
-    private ArrayList<String> mImages = new ArrayList<>();
+    private ArrayList<String> mImageNames = new ArrayList<>(); //you don't need to instantiate ArrayList
+    private ArrayList<String> mImages = new ArrayList<>(); //see above
     private Context mContext;
 
     public RecyclerViewAdapter(ArrayList<String> imagaNames, ArrayList<String> images, Context context) {
@@ -34,8 +36,8 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
 
-        ViewGroup parent = null;
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listthings, parent, false);
+//        ViewGroup parent = null;
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_listthings, viewGroup, false);
         ViewHolder holder = new ViewHolder(view);
         return holder;
     }
@@ -74,6 +76,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         CircleImageView image;
         TextView imageName;
         RelativeLayout parentLayout;
+        CardView imageCardview;
 
 
         public ViewHolder(View itemView) {
@@ -81,8 +84,19 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             image = itemView.findViewById(R.id.image);
             imageName = itemView.findViewById(R.id.image_name);
             parentLayout = itemView.findViewById(R.id.parent_layout);
+            imageCardview = itemView.findViewById(R.id.image_card_view);
+
+            imageCardview.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    sendNotification();
+                }
+
+                private void sendNotification() {
+                }
 
 
+            });
 
         }
     }
